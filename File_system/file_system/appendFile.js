46052f40be01df5aa7c6c2471a54791f15fdadab
Janhvi Bhawar <janhvibhawar28@gmail.com>
2026-01-05 14:57:18 +0530

    Delete File_system/file_system directory

--- File_system/file_system/appendFile.js
+++
@@ -1,6 +0,0 @@
-const fs = require('node:fs');
-
-fs.appendFile("./basics/file_system/data.txt", " Welcomes you", function(err){
-    if(err) console.error(err);
-    else console.log("File is appended");
-})
--- File_system/file_system/deleteFile.js
+++
@@ -1,6 +0,0 @@
-const fs = require('node:fs');
-
-fs.unlink("./basics/file_system/data.txt",function(err){
-    if(err) console.error(err);
-    else console.log("File is deleted");
-})
