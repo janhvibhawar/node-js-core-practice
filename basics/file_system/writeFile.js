80072c9b21056c384078dd3b2532ee5b17012bf2
Janhvi Bhawar <janhvibhawar28@gmail.com>
2026-01-05 14:46:34 +0530

    Delete basics/file_system directory

--- basics/file_system/writeFile.js
+++
@@ -1,6 +0,0 @@
-const fs = require('node:fs');
-
-fs.writeFile("./basics/file_system/data.txt", "Hello Node.js", function(err){
-    if(err) console.error(err);
-    else console.log("File created");
-})
