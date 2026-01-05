cf72ee3221f245b658a79993f60d342c1b2167d6
Janhvi Bhawar <janhvibhawar28@gmail.com>
2026-01-05 14:55:23 +0530

    Add writeFile function to create a text file

--- File_system/writeFile.js
+++ File_system/writeFile.js
@@ -1,1 +1,6 @@
+const fs = require('node:fs');
 
+fs.writeFile("./basics/file_system/data.txt", "Hello Node.js", function(err){
+    if(err) console.error(err);
+    else console.log("File created");
+})
